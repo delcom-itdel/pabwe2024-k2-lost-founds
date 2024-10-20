b35e8da943d1130338429a5f1ba23417c835caae
Ruth <ruth.siregar10@gmail.com>
2024-10-20 22:01:24 +0700

    add folder utils

---
+++ src/utils/api.js
@@ -0,0 +1,292 @@
+const api = (() => {
+  const BASE_URL = "https://public-api.delcom.org/api/v1";
+
+  async function _fetchWithAuth(url, options = {}) {
+    return fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+  }
+
+  function putAccessToken(token) {
+    localStorage.setItem("accessToken", token);
+  }
+
+  function getAccessToken() {
+    return localStorage.getItem("accessToken");
+  }
+
+  // API Auth => https://public-api.delcom.org/docs/1.0/api-auth
+  async function postAuthRegister({ name, email, password }) {
+    const response = await fetch(`${BASE_URL}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+    return message;
+  }
+
+  async function postAuthLogin({ email, password }) {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+    const {
+      data: { token },
+    } = responseJson;
+    return token;
+  }
+
+  // API Users => https://public-api.delcom.org/docs/1.0/apiusers
+  async function getMe() {
+    const response = await _fetchWithAuth(`${BASE_URL}/users/me`);
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+    const {
+      data: { user },
+    } = responseJson;
+    return user;
+  }
+
+  async function postChangePhotoProfile({ photoFile }) {
+    const formData = new FormData();
+    formData.append("photo", photoFile);
+    const response = await _fetchWithAuth(`${BASE_URL}/users/photo`, {
+      method: "POST",
+      body: formData,
+    });
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+    return message;
+  }
+
+  // API LostFound => https://public-api.delcom.org/docs/1.0/api/lost-founds
+  async function postAddLostFound({ title, description, status }) {
+    const response = await _fetchWithAuth(`${BASE_URL}/lost-founds`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        status,
+      }),
+    });
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+    const {
+      data: { lostfound_id },
+    } = responseJson;
+    return lostfound_id;
+  }
+
+  async function postChangeCoverLostFound({ id, cover }) {
+    const formData = new FormData();
+    formData.append("cover", cover);
+    const response = await _fetchWithAuth(
+      `${BASE_URL}/lost-founds/${id}/cover`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+    return message;
+  }
+
+  async function putUpdateLostFound({
+    id,
+    title,
+    description,
+    status,
+    is_completed,
+  }) {
+    const response = await _fetchWithAuth(`${BASE_URL}/lost-founds/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        status,
+        is_completed,
+      }),
+    });
+
+    const responseJson = await response.json();
+
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+
+    return message;
+  }
+
+  async function deleteLostFound(id) {
+    const response = await _fetchWithAuth(`${BASE_URL}/lost-founds/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+    return message;
+  }
+
+  async function getAllLostFounds() {
+    const response = await _fetchWithAuth(`${BASE_URL}/lost-founds`);
+    const responseJson = await response.json();
+
+    const { success, message, data } = responseJson;
+    if (!success) {
+      throw new Error(message);
+    }
+
+    console.log("API response:", responseJson);
+
+    const {
+      data: { lost_founds },
+    } = responseJson;
+    return lost_founds;
+  }
+
+  async function getStatsDaily() {
+    const response = await _fetchWithAuth(
+      `${BASE_URL}/lost-founds/stats/daily`
+    );
+    const responseJson = await response.json();
+
+    const { success, message, data } = responseJson;
+    if (!success) {
+      throw new Error(message);
+    }
+
+    console.log("API response:", responseJson);
+
+    const {
+      stats_lost,
+      stats_losts_completed,
+      stats_losts_process,
+      stats_founds,
+      stats_founds_completed,
+      stats_founds_process,
+    } = data;
+
+    return {
+      stats_lost,
+      stats_losts_completed,
+      stats_losts_process,
+      stats_founds,
+      stats_founds_completed,
+      stats_founds_process,
+    };
+  }
+
+  async function getStatsMonthly() {
+    const response = await _fetchWithAuth(
+      `${BASE_URL}/lost-founds/stats/monthly`
+    );
+    const responseJson = await response.json();
+
+    const { success, message, data } = responseJson;
+    if (!success) {
+      throw new Error(message);
+    }
+
+    const {
+      stats_lost,
+      stats_losts_completed,
+      stats_losts_process,
+      stats_founds,
+      stats_founds_completed,
+      stats_founds_process,
+    } = data;
+
+    return {
+      stats_lost,
+      stats_losts_completed,
+      stats_losts_process,
+      stats_founds,
+      stats_founds_completed,
+      stats_founds_process,
+    };
+  }
+
+  async function getDetailLostFound(id) {
+    const response = await _fetchWithAuth(`${BASE_URL}/lost-founds/${id}`);
+    const responseJson = await response.json();
+    const { success, message } = responseJson;
+    if (success !== true) {
+      throw new Error(message);
+    }
+
+    const {
+      data: { lost_found },
+    } = responseJson;
+    return lost_found;
+  }
+
+  return {
+    putAccessToken,
+    getAccessToken,
+    postAuthRegister,
+    postAuthLogin,
+    getMe,
+    postChangePhotoProfile,
+    postAddLostFound,
+    postChangeCoverLostFound,
+    putUpdateLostFound,
+    deleteLostFound,
+    getAllLostFounds,
+    getDetailLostFound,
+    getStatsDaily,
+    getStatsMonthly,
+  };
+})();
+
+export default api;
